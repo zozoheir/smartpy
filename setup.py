eb46e0ab656b76026f716b88e51b1ecca6b5cccb
zozoheir <42655961+zozoheir@users.noreply.github.com>
2022-04-27 02:01:47 +0000

    readded setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Smartpy',
+      version='0.0.1',
+      description='Smartpy library',
+      author='Othmane Zoheir',
+      author_email='zozoheir@umich.edu',
+      url='',
+      packages=[],
+     )
