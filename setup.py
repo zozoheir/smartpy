d3582a7b04b6d05b15daa3417a3d7702d1cea5a7
Jack Morton <jackm767@gmail.com>
2022-12-01 08:49:05 -0500

    Correct version number in setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='smartpy',
-      version='0.0.1',
+      version='0.1',
       description='Smartpy first release',
       author='Othmane Zoheir',
       author_email='zozoheir@umich.edu',
